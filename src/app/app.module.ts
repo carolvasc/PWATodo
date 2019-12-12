1557aba3c699359204379fdfbd44c62c75611b43
Carol <cvsouza04@gmail.com>
2019-12-12 20:32:06 -0300

    Alterações nas dependencias e criação do CRUD basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { TodoComponent } from './todo/todo.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
